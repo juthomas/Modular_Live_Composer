002d35aec7b1c6ebb58cbf43656f5d5aee89d70a
juthomas <juthomas@student.42.fr>
2022-03-31 18:36:41 +0200

    [ADD] Values to map

--- inc/map_input.h
+++ inc/map_input.h
@@ -31,8 +31,8 @@ typedef struct s_sensors
 	float t3_3v_current;
 	float motor_current;
 	int16_t carousel_state;
-	int16_t lid_state;
-	int32_t spectrum;
+	int16_t lid_state; //
+	int32_t spectrum;  //
 	int16_t organ_1;
 	int16_t organ_2;
 	int16_t organ_3;
@@ -70,32 +70,28 @@ static const t_map_input g_map_input[] = {
 	(t_map_input){.active = 1, .input_nu = 3, .offset = offsetof(t_sensors, photodiode_4), .data_type = FLOATING, .float_data = 2.5, .float_delta = 2.5},
 	(t_map_input){.active = 1, .input_nu = 4, .offset = offsetof(t_sensors, photodiode_5), .data_type = FLOATING, .float_data = 2.5, .float_delta = 2.5},
 	(t_map_input){.active = 1, .input_nu = 5, .offset = offsetof(t_sensors, photodiode_6), .data_type = FLOATING, .float_data = 2.5, .float_delta = 2.5},
-	// (t_data){.name = "Photodiode_1", .data_type = FLOATING, .float_data = 2.5, .float_delta = 2.5},
-	// (t_data){.name = "Photodiode_2", .data_type = FLOATING, .float_data = 2.5, .float_delta = 2.5},
-	// (t_data){.name = "Photodiode_3", .data_type = FLOATING, .float_data = 2.5, .float_delta = 2.5},
-	// (t_data){.name = "Photodiode_4", .data_type = FLOATING, .float_data = 2.5, .float_delta = 2.5},
-	// (t_data){.name = "Photodiode_5", .data_type = FLOATING, .float_data = 2.5, .float_delta = 2.5},
-	// (t_data){.name = "Photodiode_6", .data_type = FLOATING, .float_data = 2.5, .float_delta = 2.5},
-	// (t_data){.name = "Temperature_1", .data_type = FLOATING, .float_data = 0, .float_delta = 90},
-	// (t_data){.name = "Temperature_2", .data_type = FLOATING, .float_data = 0, .float_delta = 90},
-	// (t_data){.name = "Temperature_3", .data_type = FLOATING, .float_data = 0, .float_delta = 90},
-	// (t_data){.name = "Temperature_4", .data_type = FLOATING, .float_data = 0, .float_delta = 90},
-	// (t_data){.name = "Temperature_5", .data_type = FLOATING, .float_data = 0, .float_delta = 90},
-	// (t_data){.name = "Temperature_6", .data_type = FLOATING, .float_data = 0, .float_delta = 90},
-	// (t_data){.name = "Temperature_7", .data_type = FLOATING, .float_data = 0, .float_delta = 90},
-	// (t_data){.name = "Temperature_8", .data_type = FLOATING, .float_data = 0, .float_delta = 90},
-	// (t_data){.name = "Temperature_9", .data_type = FLOATING, .float_data = 0, .float_delta = 90},
-	// (t_data){.name = "Temperature_10", .data_type = FLOATING, .float_data = 0, .float_delta = 90},
+	(t_map_input){.active = 1, .input_nu = 6, .offset = offsetof(t_sensors, temperature_1), .data_type = FLOATING, .float_data = 0, .float_delta = 90},
+	(t_map_input){.active = 1, .input_nu = 6, .offset = offsetof(t_sensors, temperature_2), .data_type = FLOATING, .float_data = 0, .float_delta = 90},
+	(t_map_input){.active = 1, .input_nu = 6, .offset = offsetof(t_sensors, temperature_3), .data_type = FLOATING, .float_data = 0, .float_delta = 90},
+	(t_map_input){.active = 1, .input_nu = 6, .offset = offsetof(t_sensors, temperature_4), .data_type = FLOATING, .float_data = 0, .float_delta = 90},
+	(t_map_input){.active = 1, .input_nu = 6, .offset = offsetof(t_sensors, temperature_5), .data_type = FLOATING, .float_data = 0, .float_delta = 90},
+	(t_map_input){.active = 1, .input_nu = 6, .offset = offsetof(t_sensors, temperature_6), .data_type = FLOATING, .float_data = 0, .float_delta = 90},
+	(t_map_input){.active = 1, .input_nu = 6, .offset = offsetof(t_sensors, temperature_7), .data_type = FLOATING, .float_data = 0, .float_delta = 90},
+	(t_map_input){.active = 1, .input_nu = 6, .offset = offsetof(t_sensors, temperature_8), .data_type = FLOATING, .float_data = 0, .float_delta = 90},
+	(t_map_input){.active = 1, .input_nu = 6, .offset = offsetof(t_sensors, temperature_9), .data_type = FLOATING, .float_data = 0, .float_delta = 90},
+	(t_map_input){.active = 1, .input_nu = 6, .offset = offsetof(t_sensors, temperature_10), .data_type = FLOATING, .float_data = 0, .float_delta = 90},
+	(t_map_input){.active = 1, .input_nu = 7, .offset = offsetof(t_sensors, first_sample), .data_type = BINARY, .binary = 0},
+	(t_map_input){.active = 1, .input_nu = 8, .offset = offsetof(t_sensors, spectro_current), .data_type = FLOATING, .float_data = 0.5, .float_delta = 0.5},
+	(t_map_input){.active = 1, .input_nu = 9, .offset = offsetof(t_sensors, organ_current), .data_type = FLOATING, .float_data = 0.05, .float_delta = 0.05},
+	(t_map_input){.active = 1, .input_nu = 10, .offset = offsetof(t_sensors, vin_current), .data_type = FLOATING, .float_data = 1.25, .float_delta = 0.75},
+	(t_map_input){.active = 1, .input_nu = 11, .offset = offsetof(t_sensors, q7_current), .data_type = FLOATING, .float_data = 0.1, .float_delta = 0.1},
+	(t_map_input){.active = 1, .input_nu = 12, .offset = offsetof(t_sensors, t5v_current), .data_type = FLOATING, .float_data = 0.5, .float_delta = 0.5},
+	(t_map_input){.active = 1, .input_nu = 13, .offset = offsetof(t_sensors, t3_3v_current), .data_type = FLOATING, .float_data = 0.25, .float_delta = 0.25},
+	(t_map_input){.active = 1, .input_nu = 14, .offset = offsetof(t_sensors, motor_current), .data_type = FLOATING, .float_data = 0.025, .float_delta = 0.025},
+	(t_map_input){.active = 1, .input_nu = 15, .offset = offsetof(t_sensors, carousel_state), .data_type = INTEGER, .int_data = 180, .int_delta = 180},
+
 	// (t_data){.name = "Lid_state", .data_type = BINARY, .binary = 0},
-	// (t_data){.name = "First_sample", .data_type = BINARY, .binary = 0},
-	// (t_data){.name = "Spectro_current", .data_type = FLOATING, .float_data = 0.5, .float_delta = 0.5},
-	// (t_data){.name = "Electro_current", .data_type = FLOATING, .float_data = 1.5, .float_delta = 1.5},
-	// (t_data){.name = "Organ_current", .data_type = FLOATING, .float_data = 0.05, .float_delta = 0.05},
-	// (t_data){.name = "Vin_current", .data_type = FLOATING, .float_data = 1.25, .float_delta = 0.75},
-	// (t_data){.name = "Q7_current", .data_type = FLOATING, .float_data = 0.1, .float_delta = 0.1},
-	// (t_data){.name = "5v_current", .data_type = FLOATING, .float_data = 0.5, .float_delta = 0.5},
-	// (t_data){.name = "3.3v_current", .data_type = FLOATING, .float_data = 0.25, .float_delta = 0.25},
-	// (t_data){.name = "Motor_current", .data_type = FLOATING, .float_data = 0.025, .float_delta = 0.025},
+
 	// (t_data){.name = "Position_360", .data_type = INTEGER, .int_data = 180, .int_delta = 180},
 	// (t_data){.name = "Spectrum", .data_type = INTEGER, .int_data = 10000, .int_delta = 10000},
 	// (t_data){.name = "Organ", .data_type = FLOATING, .float_data = 2.5, .float_delta = 2.5}
