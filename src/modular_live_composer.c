4e773cfe3246836f4863fd945b8c63cdef0620c4
juthomas <juthomas@student.42.fr>
2022-03-31 19:16:51 +0200

    [AGAIN]

--- src/modular_live_composer.c
+++ src/modular_live_composer.c
@@ -634,7 +634,7 @@ void get_music_data(t_sensors *sensors)
 	// printf("Offsetof time : %d", offsetof(t_sensors, time));
 	// printf("addr of 3: %d|", (uint32_t)sensors + offsetof(t_sensors, time));
 	void *tmp = (void *)((uint32_t)sensors + offsetof(t_sensors, time));
-	(int)*tmp = 42;
+	*tmp = 42;
 	printf("Time : %d ||", sensors->time);
 	for (uint16_t i = 0; i < sensor_number; i++)
 	{
