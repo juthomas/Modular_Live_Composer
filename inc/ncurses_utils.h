ea90131d7d2d98fa4356fe2312b644d4a6da3c65
juthomas <juthomas@student.42.fr>
2022-05-19 23:47:22 +0200

    [ADD]

--- inc/ncurses_utils.h
+++ inc/ncurses_utils.h
@@ -64,7 +64,7 @@ void write_value(t_ncurses *nstruct, int index, char text)
 
 
 	// mvhline(case_y, case_x, ' ', col_width);
-	// wmove(nstruct->bottom, 1, 1);
+	wmove(nstruct->bottom, 1, 1);
 	// waddstr(nstruct->bottom, text);
 }
 
