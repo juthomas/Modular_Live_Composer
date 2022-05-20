06e9ac5618b64cc85f2b427386f18dda61ce8e5d
juthomas <juthomas@student.42.fr>
2022-05-20 11:04:15 +0200

    [FIX]

--- src/modular_live_composer.c
+++ src/modular_live_composer.c
@@ -554,8 +554,6 @@ void midi_write_multiple_euclidean(t_music_data *music_data, t_sensors *sensors_
 			);
 			if (current_euclidean_data == 0)
 			{
-				euclidean_datas[current_euclidean_data].min_chord_size = (uint8_t)map_number((uint32_t)sensors_data->temperature_3, 0, FIX_4096 - 400, 1, 3);	//temperature_3
-				euclidean_datas[current_euclidean_data].max_chord_size = (uint8_t)map_number((uint32_t)sensors_data->temperature_3, 0, FIX_4096 - 400, 1, 3);	//temperature_3
 				euclidean_datas[current_euclidean_data].octaves_size = 3;
 				euclidean_datas[current_euclidean_data].euclidean_steps_length = 24;
 				euclidean_datas[current_euclidean_data].notes_per_cycle = 4;
@@ -586,8 +584,11 @@ void midi_write_multiple_euclidean(t_music_data *music_data, t_sensors *sensors_
 		}
 	}
 
+	euclidean_datas[0].min_chord_size = (uint8_t)map_number((uint32_t)sensors_data->temperature_3, 0, FIX_4096 - 400, 1, 3);	//temperature_3
+	euclidean_datas[0].max_chord_size = (uint8_t)map_number((uint32_t)sensors_data->temperature_3, 0, FIX_4096 - 400, 1, 3);	//temperature_3
 	static uint16_t mode_requested = A2;
 	static uint16_t type_mode_requested = M_MODE_MAJOR;
+
 
 	if (sensors_data->carousel_state < 20)
 	{
