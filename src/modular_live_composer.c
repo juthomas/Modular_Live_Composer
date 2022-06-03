7d6548102cf0b0049fe13fd85f4bf354236567cd
juthomas <juthomas@student.42.fr>
2022-06-03 11:28:48 +0200

    [TEST]

--- src/modular_live_composer.c
+++ src/modular_live_composer.c
@@ -861,6 +861,13 @@ void midi_write_multiple_euclidean(t_music_data *music_data, t_sensors *sensors_
 		for (uint8_t current_euclidean_data = 0; current_euclidean_data < EUCLIDEAN_DATAS_LENGTH; current_euclidean_data++)
 		{
 			// if (current_euclidean_data == 3)
+
+	// char printf_hack[64];
+			snprintf(printf_hack, 64,"BEG WRITING CHORD %d\n", current_euclidean_data);
+			write_value(&curses_env, printf_hack);
+			snprintf(printf_hack, 64,"END WRITING CHORD %d\n", current_euclidean_data);
+
+
 			write_euclidean_step(music_data, &euclidean_datas[current_euclidean_data]);
 		}
 		// Remove chords that end this quarter division
