699f2cbd8cdf15ad67089667d0bda66719742890
juthomas <juthomas@student.42.fr>
2022-06-03 11:36:52 +0200

    [TEST]

--- src/modular_live_composer.c
+++ src/modular_live_composer.c
@@ -610,7 +610,7 @@ void midi_write_multiple_euclidean(t_music_data *music_data, t_sensors *sensors_
 				euclidean_datas[current_euclidean_data].octaves_size = 3;
 				euclidean_datas[current_euclidean_data].euclidean_steps_length = 10;
 				// euclidean_datas[current_euclidean_data].euclidean_steps_length = 15;
-				euclidean_datas[current_euclidean_data].notes_per_cycle = 2;
+				euclidean_datas[current_euclidean_data].notes_per_cycle = 7;
 				euclidean_datas[current_euclidean_data].step_gap =
 				euclidean_datas[current_euclidean_data].euclidean_steps_length / euclidean_datas[current_euclidean_data].notes_per_cycle;
 				euclidean_datas[current_euclidean_data].mess_chance = 00;
