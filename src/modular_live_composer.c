7b11284d8477a3d9bec5786db941c4087ce8df4f
juthomas <juthomas@student.42.fr>
2022-06-03 11:41:21 +0200

    [test]

--- src/modular_live_composer.c
+++ src/modular_live_composer.c
@@ -621,9 +621,9 @@ void midi_write_multiple_euclidean(t_music_data *music_data, t_sensors *sensors_
 	}
 
 
-	if (delta_shift != (uint32_t)map_number((uint32_t)sensors_data->spectro_current, 0, 33535, -14, 14))
+	if (delta_shift != (uint32_t)map_number((uint32_t)sensors_data->spectro_current, 0, 33535, -34, 34))
 	{
-		int16_t tmp = (uint32_t)map_number((uint32_t)sensors_data->spectro_current, 0,33535, -14, 14) - delta_shift;
+		int16_t tmp = (uint32_t)map_number((uint32_t)sensors_data->spectro_current, 0,33535, -34, 34) - delta_shift;
 		shift_euclidean_steps(&euclidean_datas[3], tmp);
 		delta_shift += tmp;
 	}
