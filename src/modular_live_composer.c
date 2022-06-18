074e96b1a333f4e0be82ad83e92e322066e68b2e
juthomas <juthomas@student.42.fr>
2022-06-18 11:02:26 +0200

    [ADD] 1 more octave size for 3th circle

--- src/modular_live_composer.c
+++ src/modular_live_composer.c
@@ -620,7 +620,7 @@ void midi_write_multiple_euclidean(t_music_data *music_data, t_sensors *sensors_
 			}
 			else if (current_euclidean_data == 3)
 			{
-				euclidean_datas[current_euclidean_data].octaves_size = 1;
+				euclidean_datas[current_euclidean_data].octaves_size = 2;
 				euclidean_datas[current_euclidean_data].euclidean_steps_length = 5;
 				// euclidean_datas[current_euclidean_data].euclidean_steps_length = 15;
 				euclidean_datas[current_euclidean_data].mode_beg_note = A2 - 12;
